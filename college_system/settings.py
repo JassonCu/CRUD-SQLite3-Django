3ccab215e09fbab0b129734d2f2e66310ebcb204
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-06-06 11:56:40 -0600

    feat: :zap: Add `college_system_authentication` app for auth user

--- college_system/settings.py
+++ college_system/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'college_system_authentication',
     'courses',
     'students',
     'teachers',
---
+++ college_system_authentication/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CollegeSystemAuthenticationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'college_system_authentication'
