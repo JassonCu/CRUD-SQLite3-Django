b81845715ca2ac412059dc59891d0f610c8f7dc8
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-06-24 12:48:23 -0600

    feat: :art: Add ordering and verbose_name fields in Meta class

--- college_system_authentication/models.py
+++ college_system_authentication/models.py
@@ -10,10 +10,17 @@ class Role(models.Model):
     Modelo para roles de empresas (ej. Maestro, Estudiante, etc)
     """
     objects = RoleManager()
-    name = models.CharField(_('Nombre'), max_length=50, unique=True, blank=False, null=False)
+    name = models.CharField(
+        _('Nombre'), max_length=50, unique=True, blank=False, null=False)
     name_spanish = models.CharField(
         _('Nombre Español'), max_length=50, default='', unique=True, blank=False, null=False)
-    description = models.CharField(_('Descripción'), max_length=150, unique=True, blank=False, null=False)
+    description = models.CharField(
+        _('Descripción'), max_length=150, unique=True, blank=False, null=False)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _('Rol')
+        verbose_name_plural = _('Roles')
+        ordering = ['name']
