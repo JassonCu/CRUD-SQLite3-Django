fff6f6a1887776b1b2709f255209cbe8ac9f2906
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-07-25 17:24:49 -0600

    feat: :fire: Register `RoleAdmin` and `UserAccount` models

--- college_system_authentication/admin.py
+++ college_system_authentication/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Role
+from .models import Role, UserAccount
 
-# Register your models here.admin.
-admin.site.register(Role)
+# Register your models here.
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+@admin.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'phone_number',
+                    'public_key', 'private_key')
